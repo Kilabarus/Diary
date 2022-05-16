f00cbfcff9d657bfe28b20d94de7cedda6e10eed
Kilabarus <kilabarus@gmail.com>
2022-05-16 19:59:11 +0300

    Added Model and ViewModel to WPF project

---
+++ DiaryClient/Model/TaskFrequency.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiaryClient.Model
+{
+    public class TaskFrequency
+    {
+        public TaskFrequency()
+        {
+            Tasks = new HashSet<Task>();
+        }
+
+        public long Id { get; set; }
+        public string Frequency { get; set; }
+        
+        public virtual ICollection<Task> Tasks { get; set; }
+    }
+}
---
+++ DiaryClient/Model/TaskType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiaryClient.Model
+{
+    public class TaskType
+    {
+        public TaskType()
+        {
+            Tasks = new HashSet<Task>();
+        }
+
+        public long Id { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; }
+        
+        public virtual ICollection<Task> Tasks { get; set; }
+    }
+}
