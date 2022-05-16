fd442b45d57302ca657f7b1a63140c29196aebf5
Kilabarus <kilabarus@gmail.com>
2022-05-16 14:30:05 +0300

    Generated Web API for Tasks

---
+++ DiaryClient/Model/Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace DiaryClient.Model
+{
+    public class Task
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string BeginDate { get; set; }
+        public string EndDate { get; set; }
+        public string Description { get; set; }
+        public Nullable<long> Type_Id { get; set; }
+        public Nullable<long> Frequency_Id { get; set; }
+        public Nullable<long> Status_Id { get; set; }
+
+        public virtual TaskType TaskType { get; set; }
+        public virtual TaskStatus TaskStatus { get; set; }
+        public virtual TaskFrequency TaskFrequency { get; set; }
+    }
+}
